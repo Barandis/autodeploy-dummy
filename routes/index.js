afdfd72bd637562d3a9e1d7f33119264b71d2770
Thomas Otterson <barandis1729@gmail.com>
2022-11-23 17:38:30 -0500

    Testing autodeploy

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Morgan' });
+  res.render('index', { title: 'Oska' });
 });
 
 module.exports = router;
